8d81314c11ffbe30a4fadd00f0d229f8717a94a5
Jonas <jonas.dagefoerde@gmail.com>
2013-10-10 07:43:42 +0200

    Switched to in memeory test database.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,9 @@
 require "select_options"
-# require "rspec/rails"
 
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3", 
-  :database => File.dirname(__FILE__) + "/support/select_options.sqlite3")
+  :database => ":memory:")
 
 load File.dirname(__FILE__) + '/support/schema.rb'
 load File.dirname(__FILE__) + '/support/models.rb'
