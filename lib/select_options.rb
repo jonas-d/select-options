cfd1e40efcdf890acf43068904ec31a0a03e1acd
Jonas <jonas.dagefoerde@gmail.com>
2013-10-16 08:02:42 +0200

    Replaced monkey patching by extending ActiveRecord::Base

--- lib/select_options.rb
+++ lib/select_options.rb
@@ -12,9 +12,9 @@ module SelectOptions
   class Railtie < Rails::Railtie
     initializer "select_options_init" do
       # include select options extension into ActiveRecord::Base class
-      ActiveSupport.on_load(:active_record) do
-        include SelectOptions::ActiveRecordExtension
-      end
+      # ActiveSupport.on_load(:active_record) do
+      #   include SelectOptions::ActiveRecordExtension
+      # end
       # include helpers to ActionView::Base class
       ActiveSupport.on_load(:action_view) do
         include SelectOptionsHelper
--- lib/select_options/active_record_extension.rb
+++ lib/select_options/active_record_extension.rb
@@ -1,48 +1,63 @@
 module SelectOptions
 
-  class ActiveRecordExtension
+  module ActiveRecordExtension
 
-    # keep the existing method as UnboundMethod
-    old_update_attributes = instance_method(:update_attributes)
+    extend ActiveSupport::Concern
 
-    # using define_method allows to still access the old_update_attributes method (which
-    # will be otherwise out of scope as this method has the same name)
-    # reason: define_method takes a block which encloses the surrounding lexical environment
-    define_method(:update_attributes) do |attributes|
-      seperator = "-"
-      # get all relevant associations
-      single_assoc = self.class.identify_single_through_associations
-      attributes.each do |key, value|
-        if single_assoc.index(key)
-          if value.blank?
-            # set association to nil, i.e. remove it
-            self.send(key.to_s + seperator, nil)
-          else
-            assoc_obj_class = value.split(seperator)[0].camelize
-            assoc_obj_id = value.split(seperator)[1]
-            if Object.const_defined?(assoc_obj_class) && (assoc_obj_class.constantize < ActiveRecord::Base)
-              self.send(key.to_s + "=", assoc_obj_class.constantize.find_by_id(assoc_obj_id))
-            else
-              return false
+    def self.included(base)
+      base.send :extend, ClassMethods
+      # replace_update_attributes_method(base)
+    end
+
+
+    module ClassMethods
+    
+      def replace_update_attributes_method
+        # keep the existing method as UnboundMethod
+        old_update_attributes = instance_method(:update_attributes)
+
+        # using define_method allows to still access the old_update_attributes method (which
+        # will be otherwise out of scope as this method has the same name)
+        # reason: define_method takes a block which encloses the surrounding lexical environment
+        define_method(:update_attributes) do |attributes|
+          seperator = "-"
+          # get all relevant associations
+          single_assoc = self.class.identify_single_through_associations
+          attributes.each do |key, value|
+            if single_assoc.index(key)
+              if value.blank?
+                # set association to nil, i.e. remove it
+                self.send(key.to_s + seperator, nil)
+              else
+                assoc_obj_class = value.split(seperator)[0].camelize
+                assoc_obj_id = value.split(seperator)[1]
+                if Object.const_defined?(assoc_obj_class) && (assoc_obj_class.constantize < ActiveRecord::Base)
+                  self.send(key.to_s + "=", assoc_obj_class.constantize.find_by_id(assoc_obj_id))
+                else
+                  return false
+                end
+              end
+              attributes.delete(key)
             end
           end
-          attributes.delete(key)
+          # update all other attributes
+          old_update_attributes.bind(self).call(attributes)
         end
       end
-      # update all other attributes
-      old_update_attributes.bind(self).call(attributes)
-    end
 
-    protected
-
-    # returns all ':has_one x :through y' and ':belongs_to x :through y' association names
-    def self.identify_single_through_associations
-      return reflect_on_all_associations.select { |a|
-        (a.macro == :belongs_to || a.macro == :has_one) && !a.options[:through].blank? }.map { |a|
-        a.name.to_s
-      }
-    end
+      # returns all 'has_one x :through y' association names ('belongs_to :through' does not exist)
+      def identify_single_through_associations
+        return reflect_on_all_associations.select { |a|
+          (a.macro == :has_one) && !a.options[:through].blank? }.map { |a|
+          a.name.to_s
+        }
+      end
+    end # ClassMethods
 
   end # ActiveRecordExtension
 
 end # SelectOptions
+
+
+ActiveRecord::Base.send(:include, SelectOptions::ActiveRecordExtension)
+ActiveRecord::Base.replace_update_attributes_method
